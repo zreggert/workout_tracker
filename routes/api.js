8c0710d56c3295ff6a48a723eb7437b4b1ce0823
elgalloloco <zreggert@gmail.com>
2021-06-25 21:25:28 -0400

    fixed the put route

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
-router.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
@@ -11,13 +11,24 @@ router.post("/api/workouts", ({ body }, res) => {
         });
 });
 
-router.put("/api/workouts/:id", (req, res) => {
-    Workout.findOneAndUpdate(
-        { _id: req.params.id },
-        { $push: {"exercises": req.body} },
-
+router.put("/api/workouts/:id", async (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    Workout.findByIdAndUpdate(
+        id,
+        { $push: {"exercises": body} },
+        { 
+            new: true,
+            runValidators: true
+        }
     )
-})
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+});
 
 
 router.get("/api/workouts", (req, res) => {
